727e25513c15c8eb66f06e5a03300e682dca931d
Gabe Polk <gabe.a.polk@gmail.com>
2014-08-06 09:01:59 -0700

    DB posts now update dynamically instead of manual refresh

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -5,9 +5,12 @@ app.controller('PostsCtrl', function ($scope, Post) {
   $scope.post = {url: 'http://', title: ''};
 
   $scope.submitPost = function () {
-    Post.save($scope.post)
+    Post.save($scope.post, function (ref) {
+      $scope.posts[ref.name] = $scope.post
+      $scope.post = {url: 'http://', title: ''};
+    })
     // $scope.posts.push($scope.post);
-    $scope.post = {url: 'http://', title: ''};
+    // $scope.post = {url: 'http://', title: ''};
   };
 
   $scope.posts = Post.get()
