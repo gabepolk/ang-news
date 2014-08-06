0a336e4345e69fd07f7965a68e61e34a7ce4b8d9
Gabe Polk <gabe.a.polk@gmail.com>
2014-08-06 09:10:41 -0700

    Persist post deletions on Firebase server

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,7 +1,6 @@
 'use strict';
 
 app.controller('PostsCtrl', function ($scope, Post) {
-  // $scope.posts = [];
   $scope.post = {url: 'http://', title: ''};
 
   $scope.submitPost = function () {
@@ -9,13 +8,13 @@ app.controller('PostsCtrl', function ($scope, Post) {
       $scope.posts[ref.name] = $scope.post
       $scope.post = {url: 'http://', title: ''};
     })
-    // $scope.posts.push($scope.post);
-    // $scope.post = {url: 'http://', title: ''};
   };
 
   $scope.posts = Post.get()
 
-  $scope.deletePost = function (index) {
-    $scope.posts.splice(index, 1);
+  $scope.deletePost = function (postId) {
+    Post.delete({id: postId}, function () {
+      delete $scope.posts[postId];
+    });
   };
 });
